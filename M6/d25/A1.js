b30346ae0a0fcb871055366b0bbe7d602516d701
Ambler <allinlbj@gmail.com>
2018-06-26 00:08:31 +0800

    「满足Promise规则的jQuery.ajax」

--- M6/d25/A1.js
+++ M6/d25/A1.js
@@ -6,31 +6,28 @@ window.$ = window.jQuery
 
 // 参数值用了ES6语法——解构赋值
 window.jQuery.ajax = function ({url,method,body,successFn,failFn,headers}) {
-    let request = new XMLHttpRequest()
-    request.open(method, url)
-    // 遍历头
-    for(let key in headers) {
-        let value = headers[key]
-        request.setRequestHeader(key,value)
-    }
-    request.onreadystatechange = () => {
-        if (request.readyState === 4) {
-            if (request.status >= 200 && request.status < 300) {
-                // 成功就回调successFn呗！！！
-                successFn.call(undefined, request.responseText)
-            } else if (request.status >= 400) {
-                // 失败就回调这个函数呗
-                failFn.call(undefined, request)
+    // 请记住下面这行，请记住我……虽然再见必须说……请记住我……眼泪不要坠落……
+    return new Promise(function(resove,reject) {
+        let request = new XMLHttpRequest()
+        request.open(method, url)
+        // 遍历头
+        for(let key in headers) {
+            let value = headers[key]
+            request.setRequestHeader(key,value)
+        }
+        request.onreadystatechange = () => {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                } else if (request.status >= 400) {
+                    reject.call(undefined, request)
+                }
             }
         }
-    }
-    request.send(body)
+        request.send(body)
+    })  
 }
 
-// 这两个函数，用于这样的需求「请求成功后，需要执行两个函数」，于是直接扔到那个成功的回调函数里面去了
-function f1(responseText) {}
-
-function f2(responseText) {}
 
 myButton.addEventListener('click', (e) => {
     // 用有结构的数据作为参数，如对象，这样一来，你就不会忘记「之前写的这个值是什么含义了」
@@ -50,6 +47,22 @@ myButton.addEventListener('click', (e) => {
                 console.log(x.status)
                 console.log(x.responseText)
             }
+        }
+    ).then(
+        (text) => {
+            console.log(text)
+            return 'success'
+        },
+        (request) => {
+            console.log(request)
+            return 'fail'
+        }
+    ).then(
+        (xxx) => {
+            console.log(xxx)
+        },
+        (yyy) => {
+            console.log(yyy)
         }
     )
 })
